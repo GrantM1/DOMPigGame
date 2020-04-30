9dfada276c5c7614187cc22969fe7b6f53cd26cb
Michael <GrantM1@users.noreply.github.com>
2020-04-30 19:23:43 +0200

    PigDiceGame
    Following teachings to create a PigdiceGame, learning JavaScript and its methods to accest html and styles and add logic and backend

--- app.js
+++ app.js
@@ -9,14 +9,88 @@ GAME RULES:
 
 */
 
-var scores, roundScore, activePlayer, dice;
+var scores, roundScore, activePlayer;
 
 
 scores = [0,0];
 roundScore = 0;
 activePlayer = 0;
 
-dice = Math.floor(Math.random() * 6) + 1;
-console.log(dice);
+// document.querySelector('#current-' + activePlayer).textContent = dice; // setter - set the value of #current
 
-document.querySelector('#current-0').textContent = dice;
+// document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + "<em>"; - this help to work with html code instead of puting text directly like example above
+
+// var x = document.querySelector('#score-0').textContent;
+// console.log(x); // getter - get the value to #score
+
+document.querySelector('.dice').style.display = 'none'; // set dice to not be displayed at the begginning before taping button
+
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
+
+// function btn () {
+//  // do later somthing here
+// }
+// btn(); this is global function, for this projetc better to use anonimus function which is bellow
+
+// document.querySelector('.btn-roll').addEventListener('click', btn); // this will call outside function btn, which is best way to do it, and it will execute on click as eventListener ordered to
+
+document.querySelector('.btn-roll').addEventListener('click', function() {
+ // 1. random number
+ var dice = Math.floor(Math.random() * 6) + 1;
+
+ // 2. Display the result
+ var diceDOM = document.querySelector('.dice');
+ diceDOM.style.display = 'block';
+ diceDOM.src = 'dice-' + dice + '.png';
+
+
+ // 3. Update the round score IF the roleed number was NOT a 1.
+ if (dice !== 1) {
+  // add score
+  roundScore += dice;
+  document.querySelector('#current-' + activePlayer).textContent = roundScore;
+ } else {
+  // next player
+  nextPlayer();
+
+
+ }
+
+}); // this example is called anonimus function as function execute once and without name, which can only be used here
+document.querySelector('.btn-hold').addEventListener('click', function() {
+  // Add CURRENT score to GLOBAL score
+  scores[activePlayer] += roundScore;
+
+  // update the UI
+  document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
+
+  // Check if player won the game
+  if (scores[activePlayer] >= 10) {
+   document.querySelector('#name-' + activePlayer).textContent = 'Won!';
+   document.querySelector('.dice').style.display = 'none';
+   document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
+   document.querySelector('.player-' + activePlayer + '-panel').classList.remove('activer');
+  } else {
+   // next player
+  nextPlayer();
+  }
+
+
+});
+
+
+function nextPlayer() {
+ activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+ roundScore = 0;
+
+ document.getElementById('current-0').textContent = '0'; // this set the score to 0 if 1 is diced.
+ document.getElementById('current-1').textContent = '0'; // this set the score to 0 if 1 is diced.
+
+ document.querySelector('.player-0-panel').classList.toggle('active');
+ document.querySelector('.player-1-panel').classList.toggle('active');
+
+ document.querySelector('.dice').style.display = 'none';
+}
